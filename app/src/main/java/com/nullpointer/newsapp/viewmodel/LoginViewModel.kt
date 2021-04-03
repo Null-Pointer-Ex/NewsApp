4f402befecdbe9f1796c3b750143e927673607a0
asmaaghoname <asmaaghoname204@gmail.com>
2021-04-03 16:35:31 +0200

    Create LoginViewModel.kt
    viewmodel

---
+++ app/src/main/java/com/nullpointer/newsapp/viewmodel/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.nullpointer.newsapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+
+}
